9ea661805223c233252ec9495cf404f36f2c664d
Bryan Kahiga <bryanhiggz1@gmail.com>
2021-10-13 10:49:21 +0300

    - routing for custom creator links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: ':username',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: '',
@@ -13,58 +17,59 @@ const routes: Routes = [
   },
   {
     path: 'shop',
-    loadChildren: () => import('./pages/receipt-buy/receipt-buy.module').then( m => m.ReceiptBuyPageModule)
+    loadChildren: () => import('./pages/receipt-buy/receipt-buy.module').then(m => m.ReceiptBuyPageModule)
   },
   {
     path: 'receipt/:id',
-    loadChildren: () => import('./pages/receipt/receipt.module').then( m => m.ReceiptPageModule)
+    loadChildren: () => import('./pages/receipt/receipt.module').then(m => m.ReceiptPageModule)
   },
   {
     path: 'receipt/:id/buy',
-    loadChildren: () => import('./pages/receipt-buy/receipt-buy.module').then( m => m.ReceiptBuyPageModule)
+    loadChildren: () => import('./pages/receipt-buy/receipt-buy.module').then(m => m.ReceiptBuyPageModule)
   },
   {
     path: 'checkout',
-    loadChildren: () => import('./pages/checkout/checkout.module').then( m => m.CheckoutPageModule)
+    loadChildren: () => import('./pages/checkout/checkout.module').then(m => m.CheckoutPageModule)
   },
   {
     path: 'order-confirm/:id',
-    loadChildren: () => import('./pages/order-confirm/order-confirm.module').then( m => m.OrderConfirmPageModule)
+    loadChildren: () => import('./pages/order-confirm/order-confirm.module').then(m => m.OrderConfirmPageModule)
   },
   {
     path: 'orders',
-    loadChildren: () => import('./pages/orders/orders.module').then( m => m.OrdersPageModule)
+    loadChildren: () => import('./pages/orders/orders.module').then(m => m.OrdersPageModule)
   },
   {
     path: 'orders/:id',
-    loadChildren: () => import('./pages/order-confirm/order-confirm.module').then( m => m.OrderConfirmPageModule)
+    loadChildren: () => import('./pages/order-confirm/order-confirm.module').then(m => m.OrderConfirmPageModule)
   },
   {
     path: 'receipts',
-    loadChildren: () => import('./pages/receipt-list/receipt-list.module').then( m => m.ReceiptListPageModule)
+    loadChildren: () => import('./pages/receipt-list/receipt-list.module').then(m => m.ReceiptListPageModule)
   },
   {
     path: 'dashboard/data/:entity',
-    loadChildren: () => import('./dashboard/pages/dynamic-form/dynamic-form.module').then( m => m.DynamicFormPageModule)
+    loadChildren: () => import('./dashboard/pages/dynamic-form/dynamic-form.module').then(m => m.DynamicFormPageModule)
   },
   {
     path: 'dashboard/data/:entity/:id',
-    loadChildren: () => import('./dashboard/pages/dynamic-form/dynamic-form.module').then( m => m.DynamicFormPageModule)
+    loadChildren: () => import('./dashboard/pages/dynamic-form/dynamic-form.module').then(m => m.DynamicFormPageModule)
   },
   {
     path: 'dashboard/list/:entity',
-    loadChildren: () => import('./dashboard/pages/dynamic-list/dynamic-list.module').then( m => m.DynamicListPageModule)
+    loadChildren: () => import('./dashboard/pages/dynamic-list/dynamic-list.module').then(m => m.DynamicListPageModule)
   },
   {
     path: 'suggested-products',
-    loadChildren: () => import('./pages/suggested-products/suggested-products.module').then( m => m.SuggestedProductsPageModule)
+    loadChildren: () => import('./pages/suggested-products/suggested-products.module').then(m => m.SuggestedProductsPageModule)
   },
   {
     path: 'about',
-    loadChildren: () => import('./pages/about/about.module').then( m => m.AboutPageModule)
-  },  {
+    loadChildren: () => import('./pages/about/about.module').then(m => m.AboutPageModule)
+  },
+  {
     path: 'today-orders',
-    loadChildren: () => import('./pages/today-orders/today-orders.module').then( m => m.TodayOrdersPageModule)
+    loadChildren: () => import('./pages/today-orders/today-orders.module').then(m => m.TodayOrdersPageModule)
   },
 
 ];
