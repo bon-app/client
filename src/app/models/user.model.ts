c0423c3f9b7f69180667cefc2890259fe213aed7
Bryan Higgz <bryan.kahiga@gmail.com>
2022-06-14 12:51:43 +0300

    updated user model

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,14 +1,25 @@
-import { DynamicEntity } from "../lib/dynamic-forms/decorators/dynamic-entity.decorator";
+import { DynamicEntity } from '../lib/dynamic-forms/decorators/dynamic-entity.decorator';
 
 @DynamicEntity()
 export class User {
-    public id: string;
-    public email: string;
-    public password: string;
-    public name: string;
-    public surname: string;
-    public addresses?: { street?: string, street_number?: string, city?: string, prov?: string, state?: string, note?: string, [key: string]: any }[] = [];
-    public phone: string;
-    public nickname: string;
-    public roles: string[];
+  public id: string;
+  public email: string;
+  public password: string;
+  public name: string;
+  public surname: string;
+  public addresses?: {
+    street?: string;
+    street_number?: string;
+    city?: string;
+    prov?: string;
+    state?: string;
+    note?: string;
+    [key: string]: any;
+  }[] = [];
+  public phone: string;
+  public nickname: string;
+  public roles: string[] = ['creator'];
+  public bio: string = '';
+  public image_url: string = '';
+  public verified: boolean = false;
 }
